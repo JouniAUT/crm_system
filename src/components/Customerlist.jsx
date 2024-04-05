020cd9e528321f68cabdb6a5568b3e2f72535dc2
JouniAUT <jouni.au.tuomela@gmail.com>
2024-04-05 14:42:29 +0300

    Edit customer details feature added

--- src/components/Customerlist.jsx
+++ src/components/Customerlist.jsx
@@ -5,19 +5,24 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 import { fetchCustomers } from "../customerapi";
 import AddCustomer from './AddCustomer';
+import EditCustomer from './EditCustomer';
 
 export default function Customerlist() {
 
     const [customers, setCustomers] = useState([]); //Create empty list for customers
 
     const [colDefs] = useState([ //Define AG-Grid columns
-        { field: 'firstname', filter: true, floatingFilter: true },
-        { field: 'lastname', filter: true, floatingFilter: true },
+        { field: 'firstname', filter: true, floatingFilter: true, width: 120 },
+        { field: 'lastname', filter: true, floatingFilter: true, width: 120 },
         { field: 'streetaddress', headerName: 'Address', filter: true, floatingFilter: true },
-        { field: 'postcode', filter: true, floatingFilter: true },
+        { field: 'postcode', filter: true, floatingFilter: true, width: 120 },
         { field: 'city', filter: true, floatingFilter: true },
         { field: 'email', filter: true, floatingFilter: true },
-        { field: 'phone', filter: true, floatingFilter: true },
+        { field: 'phone', filter: true, floatingFilter: true, width: 160 },
+        {
+            cellRenderer: params => <EditCustomer data={params.data} updateCustomer={updateCustomer} />,
+            width: 120
+        }
     ]);
 
     useEffect(() => {
@@ -41,6 +46,22 @@ export default function Customerlist() {
             .then(response => {
                 if (!response.ok)
                     throw new Error('Error when adding new customer ' + response.statusText)
+                return response.json();
+            })
+            .then(() => handleFetch())
+            .catch(err => console.error(err))
+    }
+
+    const updateCustomer = (url, updateCustomer) => {
+        fetch(url, {
+            method: 'PUT',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(updateCustomer)
+        })
+
+            .then(response => {
+                if (!response.ok)
+                    throw new Error("Error when updating customer details " + response.statusText)
                 return response.json();
             })
             .then(() => handleFetch())
