9ad24b543863223ddaa95f2830648370692868ea
JouniAUT <jouni.au.tuomela@gmail.com>
2024-04-05 11:55:39 +0300

    Place for statistics and calendar added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Container from '@mui/material/Container';
 import Customerlist from './components/Customerlist';
 import Traininglist from './components/Trainginlist';
 import Home from './components/Home';
+import Calendar from './components/Calendar';
+import Statistics from './components/Statistics';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 
@@ -23,11 +25,15 @@ function App() {
           <Tab value='one' label='Home' justifycontent='center' alignitems='center' />
           <Tab value='two' label='Customers' justifycontent='center' alignitems='center' />
           <Tab value='three' label='Trainings' justifycontent='center' alignitems='center' />
+          <Tab value='four' label='Statistics' justifycontent='center' alignitems='center' />
+          <Tab value='five' label='Calendar' justifycontent='center' alignitems='center' />
         </Tabs>
 
         {currentValue === 'one' && <Home />}
         {currentValue === 'two' && <Customerlist />}
         {currentValue === 'three' && <Traininglist />}
+        {currentValue === 'four' && <Statistics />}
+        {currentValue === 'five' && <Calendar />}
 
       </Container>
     </>
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,17 @@
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+export default function Calendar() {
+    return (
+        <>
+            <Card sx={{ minWidth: 275, maxWidth: 500 }}>
+                <CardContent>
+                    <Typography>
+                        There will be Calendar soon
+                    </Typography>
+                </CardContent>
+            </Card>
+        </>
+    );
+}
